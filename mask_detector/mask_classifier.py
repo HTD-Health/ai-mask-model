9e97981fe0ac066bf29d980a9c6a369e011b0adf
Remigiusz Poltorak <remigiusz.poltorak@htdevelopers.com>
2021-01-23 00:08:58 +0100

    Add CLI params support

--- mask_detector/mask_classifier.py
+++ mask_detector/mask_classifier.py
@@ -49,9 +49,24 @@ class MaskClassifier(LightningModule):
         # self.hparams available because we called self.save_hyperparameters()
         return Adam(self.parameters(), lr=self.learning_rate)
 
+    @staticmethod
+    def add_model_specific_args(parent_parser):
+        parser = ArgumentParser(parents=[parent_parser], add_help=False)
+        parser.add_argument('--learning_rate', type=float, default=1e-3)
+        return parser
+
 
 def cli_main():
     seed_everything(1234)
+
+    # ------------
+    # args
+    # ------------
+    parser = ArgumentParser()
+    parser.add_argument('--batch_size', default=32, type=int)
+    parser = Trainer.add_argparse_args(parser)
+    parser = MaskClassifier.add_model_specific_args(parser)
+    args = parser.parse_args()
 
     # ------------
     # data
@@ -60,22 +75,20 @@ def cli_main():
     ds_train, ds_validate, ds_test = train_val_test_split(
         dataset, train_ratio=0.8, validate_ratio=0.1, test_ratio=0.1)
 
-    train_loader = DataLoader(ds_train, batch_size=32)
-    val_loader = DataLoader(ds_validate, batch_size=32)
-    test_loader = DataLoader(ds_test, batch_size=32)
+    train_loader = DataLoader(ds_train, batch_size=args.batch_size)
+    val_loader = DataLoader(ds_validate, batch_size=args.batch_size)
+    test_loader = DataLoader(ds_test, batch_size=args.batch_size)
 
     # ------------
     # model
     # ------------
     net = BasicCNN()
-    model = MaskClassifier(net, learning_rate=0.0001)
+    model = MaskClassifier(net, learning_rate=args.learning_rate)
 
     # ------------
     # training
     # ------------
-    gpus = 1 if torch.cuda.is_available() else 0
-
-    trainer = Trainer(gpus=gpus, max_epochs=3)
+    trainer = Trainer.from_argparse_args(args)
     trainer.fit(model, train_loader, val_loader)
 
     # ------------
