67921e4ecc094a761e820c53e1ac91abf67973ff
Jedrzej Polaczek <jedrzej.polaczek@gmail.com>
2021-06-15 15:35:13 +0200

    feat: resolve conflicts with setup libraries versions.

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ setup(
     author='HTD_AI_Team',
     packages=find_packages(include=['mask_detector', 'mask_detector.*']),
     install_requires=[
-        'numpy==1.19.5',
-        'pytorch-lightning==1.1.4',
-        'torch==1.7.1',
-        'torchvision==0.8.2',
-        'tqdm==4.56.0',
-        'pandas==1.2.0',
-        'scikit-learn==0.24.0',
-        'opencv-python==4.5.1.48',
-        'pillow==8.1.0'
+        'numpy==1.20.3',
+        'pytorch-lightning==1.3.4',
+        'torch==1.8.1',
+        'torchvision==0.9.1',
+        'tqdm==4.41.0',
+        'pandas==1.2.4',
+        'scikit-learn==0.24.2',
+        'pillow==8.2.0',
+        'opencv-python==4.5.2.54'
     ]
 )
