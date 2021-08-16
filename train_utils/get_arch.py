72c05de7e2d32cb2626b99af08aebe4968457fe2
Jedrzej Polaczek <jedrzej.polaczek@gmail.com>
2021-08-16 15:05:25 +0200

    fix: review issues.

--- train_utils/get_arch.py
+++ train_utils/get_arch.py
@@ -12,13 +12,18 @@ def get_arch(arch: str) -> torchvision.models:
     Return:
         torchvision.models: object of the proper architecture class.
     """
-    if arch == 'resnet18':
-        model = torchvision.models.resnet18(pretrained=True)
-    if arch == 'alexnet':
-        model = torchvision.models.alexnet(pretrained=True)
-    if arch == 'vgg13':
-        model = torchvision.models.vgg13(pretrained=True)
-    if arch == 'vgg16':    
-        model = torchvision.models.vgg16(pretrained=True)
+    try:
+        if arch == 'resnet18':
+            model = torchvision.models.resnet18(pretrained=True)
+        elif arch == 'alexnet':
+            model = torchvision.models.alexnet(pretrained=True)
+        elif arch == 'vgg13':
+            model = torchvision.models.vgg13(pretrained=True)
+        elif arch == 'vgg16':
+            model = torchvision.models.vgg16(pretrained=True)
+        else:
+            raise ValueError(f"No matching architecture: {arch}")
+    except ValueError as e:
+        print(e)
     
     return model
